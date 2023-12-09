169766940e864f647f156ac434479e7303bccea8
Intellectcodeofficial <intellectcodeofficial@gmail.com>
2023-12-09 17:55:21 +0545

    Changes

--- GenTechNepal.API/Data/Implementaion/UserServices.cs
+++ GenTechNepal.API/Data/Implementaion/UserServices.cs
@@ -5,10 +5,12 @@ namespace GenTechNepal.API.Data.Implementaion
 {
     public class UserServices : IuserService
     {
-
-        public UserServices()
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _environment;
+        public UserServices(ApplicationDbContext context, IWebHostEnvironment environment)
         {
-            
+            _context = context;
+            _environment = environment;
         }
         public Task<User> AddAsync(User user, IFormFile file)
         {
